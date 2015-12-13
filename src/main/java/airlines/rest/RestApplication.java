74c25d0052e65d485111f578252e9262e785323f
winio94 <winio94@gmail.com>
2015-12-13 16:36:49 +0100

    add main rest application class

---
+++ src/main/java/airlines/rest/RestApplication.java
@@ -0,0 +1,12 @@
+package airlines.rest;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by winio_000 on 2015-12-13.
+ */
+public class RestApplication extends Application {
+
+    public RestApplication() {
+    }
+}
