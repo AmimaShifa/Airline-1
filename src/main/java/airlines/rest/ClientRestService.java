20c8a9232afc484783376a77a9522d97131ac8f6
winio94 <winio94@gmail.com>
2015-12-13 16:37:11 +0100

    create client REST service

---
+++ src/main/java/airlines/rest/ClientRestService.java
@@ -0,0 +1,69 @@
+package airlines.rest;
+
+import airlines.model.Client;
+import airlines.service.ClientService;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+/**
+ * Created by winio_000 on 2015-12-13.
+ */
+@Named
+@Path("/clients")
+public class ClientRestService {
+
+    @Inject
+    private ClientService clientService;
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getClient(@PathParam("id") long id) {
+        Client client = clientService.findOne(id);
+        if (client == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Client not found for id : " + id).build();
+        }
+        return Response.ok(client, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Client> getClients() {
+        return (List<Client>) clientService.findAll();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteClient(@PathParam("id") long id) {
+        clientService.delete(id);
+        return Response.status(200).entity("Client deleted").build();
+    }
+
+    @DELETE
+    public Response deleteAllClients() {
+        clientService.deleteAll();
+        return Response.status(200).entity("All clients deleted").build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createClient(Client client) {
+        try {
+            clientService.save(client);
+        } catch (DataIntegrityViolationException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Client with this PESEL/EMAIL  already exists").build();
+        }
+        return Response.status(200).entity("Client created").build();
+    }
+
+    public void setClientService(ClientService clientService) {
+        this.clientService = clientService;
+    }
+}
