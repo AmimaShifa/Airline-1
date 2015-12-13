1461201fefe4c5042bca0b0d8557ef13bdad6043
winio94 <winio94@gmail.com>
2015-12-13 16:32:31 +0100

    create client DAO interface

---
+++ src/main/java/airlines/service/ClientService.java
@@ -0,0 +1,21 @@
+package airlines.service;
+
+import airlines.model.Client;
+
+/**
+ * Created by winio_000 on 2015-12-13.
+ */
+public interface ClientService {
+
+    void deleteAll();
+
+    void delete(Client client);
+
+    void delete(long id);
+
+    void save(Client client);
+
+    Iterable<Client> findAll();
+
+    Client findOne(long id);
+}
