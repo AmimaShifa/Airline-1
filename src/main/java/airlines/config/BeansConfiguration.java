00fba624d5a4d75ffa1e435d8cfa839768144809
winio94 <winio94@gmail.com>
2015-12-13 16:19:05 +0100

    create beans configuration

---
+++ src/main/java/airlines/config/BeansConfiguration.java
@@ -0,0 +1,10 @@
+package airlines.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by winio_000 on 2015-12-13.
+ */
+@Configuration
+public class BeansConfiguration {
+}
