8dbdd8b4d0b7f85551c10f176a36ed0a3d188cbc
winio94 <winio94@gmail.com>
2015-12-13 09:42:18 +0100

    create project structure

---
+++ scripts/airlines.sql
@@ -0,0 +1,2 @@
+create database airlines;
+use airlines;
---
+++ src/main/java/airlines/controller/HomeController.java
@@ -0,0 +1,17 @@
+package airlines.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by winio_000 on 2015-11-02.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+
+}
