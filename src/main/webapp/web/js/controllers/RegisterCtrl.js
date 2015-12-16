95b323dcf5c51a6ea2d116ea74b7cb2355eff246
winio94 <winio94@gmail.com>
2015-12-16 20:31:36 +0100

    implement adding client after registration

--- src/main/webapp/web/js/controllers/RegisterCtrl.js
+++ src/main/webapp/web/js/controllers/RegisterCtrl.js
@@ -1,6 +1,20 @@
 /**
  * Created by winio_000 on 2015-12-15.
  */
-app.controller("RegisterCtrl", function ($scope) {
+app.controller('RegisterCtrl', ['$scope', '$controller', function ($scope, $controller) {
+    var clientCtrlScope = $scope.$new();
+    $controller('ClientCtrl', {$scope: clientCtrlScope});
 
-});
+    $scope.loggedIn = false;
+
+    $scope.afterLogIn = function (client) {
+        if ($scope.loggedIn == true) {
+            alert("Dziękujemy za utworzenie konta");
+            clientCtrlScope.addClient(client);
+        }
+    };
+
+    $scope.setLoginTrue = function () {
+        $scope.loggedIn = true;
+    };
+}]);
