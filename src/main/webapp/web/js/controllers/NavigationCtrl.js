5f8923745d7aea5967126d874db538603ba3b640
winio94 <winio94@gmail.com>
2015-12-18 01:13:18 +0100

    names changes

--- src/main/webapp/web/js/controllers/NavigationCtrl.js
+++ src/main/webapp/web/js/controllers/NavigationCtrl.js
@@ -1,22 +1,22 @@
 /**
  * Created by winio_000 on 2015-12-13.
  */
-app.controller('NavigationCtrl', function($scope, $http) {
+app.controller('NavigationCtrl', function ($scope, $http) {
 
 // functions ////////////////////////////////////////
-    $scope.getNavigationMap = function(){
-        $http.get('menu.json').success(function(response) {
+    $scope.getNavigationMap = function () {
+        $http.get('menu.json').success(function (response) {
             $scope.navigationMap = response;
         });
     };
 
-    $scope.navigateTo = function($navigationPart){
+    $scope.navigateTo = function ($navigationPart) {
         if (typeof $navigationPart.url != 'undefined') {
             $scope.includedPartUrl = $navigationPart.url;
         }
     };
 //////////////////////////////////////////////////////
-    $scope.includedPartUrl = "partials/SummaryPage.html";
+    $scope.includedPartUrl = "partials/AdministrationPage.html";
     $scope.navigationMap = [];
     $scope.getNavigationMap();
 });
