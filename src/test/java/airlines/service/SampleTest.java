d752f6d83ea2628bbd2de11a09b9bc2c46c86b92
winio94 <winio94@gmail.com>
2015-12-18 01:14:21 +0100

    add sample test for testing surefire plugin

---
+++ src/test/java/airlines/service/SampleTest.java
@@ -0,0 +1,19 @@
+package airlines.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by winio_000 on 2015-12-18.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue("0".equals("0"));
+    }
+}
