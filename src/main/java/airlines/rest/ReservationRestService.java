e741b37b61bf9cb6d9a6c1c629c4cf74bde5a709
winio94 <winio94@gmail.com>
2016-01-06 18:27:16 +0100

    add REST service for reservations

---
+++ src/main/java/airlines/rest/ReservationRestService.java
@@ -0,0 +1,72 @@
+package airlines.rest;
+
+import airlines.model.Reservation;
+import airlines.service.ReservationService;
+import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * Created by winio_000 on 2016-01-05.
+ */
+
+@Named
+@Path("/reservations")
+public class ReservationRestService {
+
+    private static final Logger LOGGER = Logger.getLogger(ReservationRestService.class);
+
+    @Inject
+    private ReservationService reservationService;
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getReservation(@PathParam("id") Long id) {
+        Reservation reservation = reservationService.findOne(id);
+        if (reservation == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Reservation not found for id : " + id).build();
+        }
+        return Response.ok(reservation, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Reservation> getReservations() {
+        return (List<Reservation>) reservationService.findAll();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response deleteReservation(@PathParam("id") Long id) {
+        try {
+            reservationService.delete(id);
+            return Response.status(200).entity("Reservation deleted").build();
+        } catch (DataAccessException de) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Could not delete reservation for id : " + id + ", such reservation not found")
+                    .build();
+        }
+    }
+
+    @DELETE
+    public Response deleteAllReservations() {
+        try {
+            reservationService.deleteAll();
+            return Response.status(200).entity("All Reservations deleted").build();
+        } catch (DataAccessException de) {
+            return Response.status(Response.Status.NOT_FOUND).entity("There is no reservations to delete").build();
+        }
+
+    }
+
+    public void setReservationService(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+}
