59d91f8ad7e52e5d1c3456932b6d467840249473
winio94 <winio94@gmail.com>
2016-01-06 18:30:19 +0100

    add controller for reservation

--- src/main/webapp/web/js/controllers/ReservationCtrl.js
+++ src/main/webapp/web/js/controllers/ReservationCtrl.js
@@ -1,7 +1,22 @@
 /**
  * Created by winio_000 on 2015-12-15.
  */
-app.controller('ReservationCtrl', function($scope) {
+app.controller('ReservationCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
 
+    $scope.departureDate = $filter('date')(new Date(), 'dd-MM-yyyy');
+    $scope.arrivalDate = $filter('date')(new Date(new Date().getTime() + (3 * 60 * 60 * 24 * 1000)), 'dd-MM-yyyy');
 
-});
+    //MAKE RESERVATION
+    $scope.makeReservation = function (clientId) {
+        $http({
+            method: 'POST',
+            url: '/airlines/reservations',
+            data: {}
+        }).then(function successCallback(response) {
+            console.log("Reserwacja udana");
+        }, function errorCallback() {
+            console.log("Reserwacja nieudana! ");
+        });
+    };
+
+}]);
