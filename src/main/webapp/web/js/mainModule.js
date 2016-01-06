90be3f8cf3e2823b43917e82165570ad63c9ca13
winio94 <winio94@gmail.com>
2016-01-06 18:03:40 +0100

    add ui.bootstrap  for date picker

--- src/main/webapp/web/js/mainModule.js
+++ src/main/webapp/web/js/mainModule.js
@@ -1,25 +1,25 @@
 /**
  * Created by winio_000 on 2015-12-13.
  */
-var app = angular.module('mainModule', []);
+var app = angular.module('mainModule', ['ui.bootstrap']);
 
-app.directive('navigationBar', function() {
+app.directive('navigationBar', function () {
     return {
         restrict: 'E',
         templateUrl: 'partials/NavigationBar.html'
     }
 });
 
-app.directive('clientAddForm', function() {
-   return {
-       restrict: 'E',
-       templateUrl: 'partials/ClientAddForm.html'
-   }
+app.directive('clientAddForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/ClientAddForm.html'
+    }
 });
 
 app.directive('flightAddForm', function () {
-   return {
-       restrict: 'E',
-       templateUrl: 'partials/FlightAddForm.html'
-   }
+    return {
+        restrict: 'E',
+        templateUrl: 'partials/FlightAddForm.html'
+    }
 });
