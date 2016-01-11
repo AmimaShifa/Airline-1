072a002bdbe6a15a041d9c03f20c222efd2cb67b
winio94 <winio94@gmail.com>
2016-01-11 23:07:27 +0100

    get paginated flights in reservation controller

--- src/main/webapp/web/js/controllers/ReservationCtrl.js
+++ src/main/webapp/web/js/controllers/ReservationCtrl.js
@@ -1,10 +1,10 @@
 /**
  * Created by winio_000 on 2015-12-15.
  */
-app.controller('ReservationCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
+app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', function ($scope, $http, $filter, $controller) {
 
-    $scope.departureDate = $filter('date')(new Date(), 'dd-MM-yyyy');
-    $scope.arrivalDate = $filter('date')(new Date(new Date().getTime() + (3 * 60 * 60 * 24 * 1000)), 'dd-MM-yyyy');
+    $scope.departureDate = $filter('date')(new Date(), 'yyyy-MM-dd');
+    $scope.arrivalDate = $filter('date')(new Date(new Date().getTime() + (3 * 60 * 60 * 24 * 1000)), 'yyyy-MM-dd');
 
     //MAKE RESERVATION
     $scope.makeReservation = function (clientId) {
@@ -19,4 +19,10 @@ app.controller('ReservationCtrl', ['$scope', '$http', '$filter', function ($scop
         });
     };
 
+    var flightControllerScope = $scope.$new();
+    $controller('FlightCtrl', {$scope: flightControllerScope});
+
+    $scope.getFlights = function(source, destination, arrival, departure) {
+        flightControllerScope.getFlghtsUsingQueryParams(source, destination, arrival, departure);
+    };
 }]);
