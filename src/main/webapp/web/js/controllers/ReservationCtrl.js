37e1b8185a5fc24c62f1e30cbfbb94b6955a1e64
winio94 <winio94@gmail.com>
2016-01-16 13:44:46 +0100

    searching flights works

--- src/main/webapp/web/js/controllers/ReservationCtrl.js
+++ src/main/webapp/web/js/controllers/ReservationCtrl.js
@@ -1,10 +1,12 @@
 /**
  * Created by winio_000 on 2015-12-15.
  */
-app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', function ($scope, $http, $filter, $controller) {
+app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', '$timeout', function ($scope, $http, $filter, $controller, $timeout) {
 
     $scope.departureDate = $filter('date')(new Date(), 'yyyy-MM-dd');
     $scope.arrivalDate = $filter('date')(new Date(new Date().getTime() + (3 * 60 * 60 * 24 * 1000)), 'yyyy-MM-dd');
+    $scope.controllerName = 'ReservationCtrl';
+    $scope.flights = [];
 
     //MAKE RESERVATION
     $scope.makeReservation = function (clientId) {
@@ -22,7 +24,14 @@ app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', 
     var flightControllerScope = $scope.$new();
     $controller('FlightCtrl', {$scope: flightControllerScope});
 
-    $scope.getFlights = function(source, destination, arrival, departure) {
+    $scope.getFlights = function (source, destination, arrival, departure) {
         flightControllerScope.getFlghtsUsingQueryParams(source, destination, arrival, departure);
+        //TODO replace this with promises!!!!
+        $timeout(function () {
+            setProcessedFlights();
+        }, 100);
+    };
+    var setProcessedFlights = function () {
+        $scope.flights = flightControllerScope.flights;
     };
 }]);
