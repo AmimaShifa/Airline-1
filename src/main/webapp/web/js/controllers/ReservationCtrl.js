20c0ddf92666e165406cd3173acda8b918f9e90d
winio94 <winio94@gmail.com>
2016-01-25 22:21:26 +0100

    add dynamic css for reservation page

--- src/main/webapp/web/js/controllers/ReservationCtrl.js
+++ src/main/webapp/web/js/controllers/ReservationCtrl.js
@@ -2,11 +2,11 @@
  * Created by winio_000 on 2015-12-15.
  */
 app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', '$timeout', function ($scope, $http, $filter, $controller, $timeout) {
-
     $scope.departureDate = $filter('date')(new Date(), 'yyyy-MM-dd');
     $scope.arrivalDate = $filter('date')(new Date(new Date().getTime() + (3 * 60 * 60 * 24 * 1000)), 'yyyy-MM-dd');
     $scope.controllerName = 'ReservationCtrl';
-    $scope.flights = [];
+    $scope.labelColumnSize = 3;
+    $scope.selectColumnSize = 3;
 
     //MAKE RESERVATION
     $scope.makeReservation = function (clientId) {
@@ -31,7 +31,37 @@ app.controller('ReservationCtrl', ['$scope', '$http', '$filter', '$controller', 
             setProcessedFlights();
         }, 100);
     };
+
     var setProcessedFlights = function () {
         $scope.flights = flightControllerScope.flights;
+        if ($scope.flights.length == 0) {
+            $scope.labelColumnSize = 3;
+            $scope.selectColumnSize = 3;
+            $scope.reservationContainerStyle = {};
+            $scope.reservationResultStyle = {};
+            $scope.formHorizontalStyle = {};
+            $scope.containerStyle = {};
+        } else {
+            $scope.labelColumnSize = 4;
+            $scope.selectColumnSize = 7;
+
+            $scope.reservationContainerStyle = {
+                display: 'inline-flex'
+            };
+
+            $scope.reservationResultStyle = {
+                padding: "0%",
+                margin: "0 auto",
+                'max-width': "200px"
+            };
+
+            $scope.formHorizontalStyle = {
+                'min-width': "400px"
+            };
+
+            $scope.containerStyle = {
+                'padding-left': "0px"
+            };
+        }
     };
 }]);
